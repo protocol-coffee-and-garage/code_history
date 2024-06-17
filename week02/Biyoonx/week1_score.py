cddbdc2acd1c3716cd692b8b1a3aef84cfc9f67e
biyoonx <ok2geoya@gmail.com>
2024-06-17 19:17:24 +0900

    update week1_score.py

--- week02/Biyoonx/week1_score.py
+++ week02/Biyoonx/week1_score.py
@@ -1,17 +1,17 @@
 import sys
 read = sys.stdin.readline
 
-num_line = int(read())
+score_var1 = int(read())
 def score(num:int):
     grade_table = {10: 'A', 9: 'A', 8: 'B', 7: 'C', 6: 'D'}
     if num // 10 in grade_table:
         return grade_table.get(num // 10)
     else:
         return 'F'
-print(score(num_line))
+print(score(score_var1))
 
 # 기존 답변
-score_var = int(input())
+score_var2 = int(input())
 def grade(score):
 		if (90 <= score <= 100):
 				return 'A'
@@ -23,4 +23,4 @@ def grade(score):
 				return 'D'
 		else:
 				return 'F'
-print(grade(score_var))
+print(grade(score_var2))
