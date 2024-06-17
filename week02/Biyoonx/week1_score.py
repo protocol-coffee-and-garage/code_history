ffe163fe22fed01463f6a76e78c7212954562494
biyoonx <ok2geoya@gmail.com>
2024-06-17 19:15:22 +0900

    add week1_score.py

---
+++ week02/Biyoonx/week1_score.py
@@ -0,0 +1,26 @@
+import sys
+read = sys.stdin.readline
+
+num_line = int(read())
+def score(num:int):
+    grade_table = {10: 'A', 9: 'A', 8: 'B', 7: 'C', 6: 'D'}
+    if num // 10 in grade_table:
+        return grade_table.get(num // 10)
+    else:
+        return 'F'
+print(score(num_line))
+
+# 기존 답변
+score_var = int(input())
+def grade(score):
+		if (90 <= score <= 100):
+				return 'A'
+		elif (80 <= score <= 89):
+				return 'B'
+		elif (70 <= score <= 79):
+				return 'C'
+		elif (60 <= score <= 69):
+				return 'D'
+		else:
+				return 'F'
+print(grade(score_var))
