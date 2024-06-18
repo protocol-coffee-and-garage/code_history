e7c33f1220710103f91045272d3c2b29bea1c5e3
pOcOq38 <cylee0322@gmail.com>
2024-06-18 20:21:25 +0900

    week1 commit

---
+++ week01/Chae/main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args)  throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(bf.readLine());
+//        int n = Integer.parseInt(bf.readLine());
+
+        System.out.printf("이건 숫자 %d, 이건 문자 %c %s", 4, 'a', "asdf");
+        System.out.println(String.format("이건 숫자 %d, 이건 문자 %c %s", 4, 'a', "asdf"));
+//        new ArrayList<>(Arrays.asList("a", "n", "ccc")).stream()
+
+        }
+    }
+
+
