e9fd22be92979414bedd0852142293f3e19ac5f3
biyoonx <ok2geoya@gmail.com>
2024-07-08 18:39:10 +0900

    update week3_findMin(11003).py

--- Biyoonx/week03/week3_findMin(11003).py
+++ Biyoonx/week03/week3_findMin(11003).py
@@ -1,2 +1,15 @@
 import sys
 read = sys.stdin.readline
+
+n, l = map(int, read().split())
+a_list = [*map(int, read().split())]
+# 어려워....모르겟어....
+result = []
+min_num = float("inf")
+for idx2 in range(n):
+    target = a_list[(idx2 - l) if idx2 >= l else 0:idx2 + 1]
+    target_min = min(target)
+    min_num = min(target_min, min_num) if target_min  > 0 else min_num
+    result.append(str(min_num))
+
+print(' '.join(result))
