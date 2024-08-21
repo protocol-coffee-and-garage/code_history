4bc8c7e614b6a47df457c6bab743de651d988947
biyoonx <ok2geoya@gmail.com>
2024-08-22 08:26:51 +0900

    update week5_treasure(1026).py

--- Biyoonx/week05/sort/week5_treasure(1026).py
+++ Biyoonx/week05/sort/week5_treasure(1026).py
@@ -1,7 +1,16 @@
+from functools import reduce
 import sys
 read = sys.stdin.readline
 
-# s = int(read())
-# a_list = [*map(int, read().split())]
-# b_list = list(map(lambda (i, v): (int(v), i), enumerate(read().split())))
-# print(b_list)
+s = int(read())
+a_list = [*map(int, read().split())]
+b_list = [*map(int, read().split())]
+
+sorted_a_list = sorted(a_list)
+sorted_b_list = sorted(b_list, reverse = True)
+
+# v1
+# print(sum(map(lambda a: a[0] * a[1], [*zip(sorted_a_list, sorted_b_list)])))
+
+# v2
+print(reduce(lambda a, b: a + b, [x * y for x, y in zip(sorted_a_list, sorted_b_list)]))
